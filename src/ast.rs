46f8ef269ff503a0ea3e9d7fbc20f76200a7e0a3
Piotr Domagalski <pdomagalski02@gmail.com>
2024-11-22 14:45:08 +0100

    ParsingError builders, Display trait, and tests

--- src/ast.rs
+++ src/ast.rs
@@ -8,7 +8,9 @@ impl<T: Iterator<Item = Token>> TokenIter for T {}
 mod program;
 mod statement;
 mod expression;
+mod parsing_error;
 
 pub use program::*;
 pub use statement::*;
 pub use expression::*;
+pub use parsing_error::*;
--- src/ast/parsing_error.rs
+++ src/ast/parsing_error.rs
@@ -2,8 +2,8 @@ use crate::token::Token;
 
 pub enum ParsingError {
     UnexpectedToken {
-        got: Token,
-        expected: Token,
+        got: Option<Token>,
+        expected: Vec<Token>,
         parsing_what: &'static str,
     },
     MultipleErrors {
@@ -13,7 +13,208 @@ pub enum ParsingError {
         message: &'static str
     }
 }
+//builders
+impl ParsingError {
+    pub fn new_unexpected(got: Option<&Token>, expected: Vec<Token>, parsing_what: &'static str) -> ParsingError {
+        let got = match got {
+            None => None,
+            Some(tok) => Some(tok.clone())
+        };
+        ParsingError::UnexpectedToken { got, expected, parsing_what}
+    }
+    pub fn new_multiple(errors: Vec<ParsingError>) -> ParsingError {
+        ParsingError::MultipleErrors { errors }
 
+    }
+
+    pub fn new_other(message: &'static str) -> ParsingError {
+        ParsingError::OtherError { message }
+    }
+}
+
+impl From<ParsingError> for &str {
+    fn from(value: ParsingError) -> Self {
+        value.to_string().leak()
+    }
+}
+
+//display helpers
+impl ParsingError {
+    fn display_option_token( option_token: &Option<Token>) -> String {
+        match option_token {
+            Some(got) => {
+                got.to_string()
+            }
+            None =>
+                "None".to_string()
+        }
+    }
+    fn display_vec_token( vec_token: &Vec<Token>) -> String {
+        match vec_token.len() {
+            0 => "None".to_string(),
+            1 => vec_token[0].to_string(),
+            _ => {
+                vec_token.iter()
+                    .map(|tok| {let mut newtok = tok.to_string(); newtok})
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            }
+        }
+    }
+}
+
+use std::fmt::{Display, Formatter};
+impl Display for ParsingError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnexpectedToken { got, expected, parsing_what } => {
+                let got = Self::display_option_token(&got);
+                let expected = Self::display_vec_token(&expected);
+                write!(f, "Unexpected token while parsing {parsing_what} - got: {got}, expected: {expected}")
+            },
+            Self::MultipleErrors { errors } => {
+                write!(f, "Multiple errors - len={}", errors.len())
+                // TODO: display all the errors, not just count
+            },
+            Self::OtherError { message } => {
+                if message.len() == 0 {
+                    write!(f, "Other error")
+                } else {
+                    write!(f, "Other error: {message}")
+                }
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display_option_token() {
+        struct Test {
+            input: Option<Token>,
+            expected: String
+        };
+        let tests = [
+            Test {
+                input: Option::Some(Token::Bang),
+                expected: "!".to_string(),
+            },
+            Test {
+                input: Option::None,
+                expected: "None".to_string(),
+            }
+        ];
+
+        for Test{input, expected} in tests {
+            assert_eq!(ParsingError::display_option_token(&input), expected);
+        }
+    }
+
+    #[test]
+    fn test_display_vec_token() {
+        struct Test {
+            input: Vec<Token>,
+            expected: String
+        };
+
+        let tests = [
+            Test {
+                input: vec![Token::Bang, Token::Plus, Token::Function],
+                expected: format!("{}, {}, {}", Token::Bang, Token::Plus, Token::Function),
+            },
+            Test {
+                input: vec![Token::Equals],
+                expected: Token::Equals.to_string(),
+            },
+            Test {
+                input: vec![],
+                expected: "None".to_string(),
+            }
+        ];
+
+        for Test{input, expected} in tests {
+            assert_eq!(ParsingError::display_vec_token(&input), expected);
+        }
+    }
+
+    #[test]
+    fn test_display_parsing_error() {
+        struct Test {
+            input: ParsingError,
+            expected: String,
+        };
+
+        let tests = [
+            //unexpected: got - None and Some, expected - empty, 1, 3
+            Test {
+                input: ParsingError::new_unexpected(
+                    None,
+                    vec![Token::Semicolon],
+                    "test1"),
+                expected: format!("Unexpected token while parsing {} - got: {}, expected: {}",
+                    "test1",
+                    ParsingError::display_option_token(&None),
+                    ParsingError::display_vec_token(&vec![Token::Semicolon])),
+            },
+            Test {
+                input: ParsingError::new_unexpected(
+                    Some(&Token::Identifier("barbaz".to_string())),
+                    vec![Token::Comma, Token::Semicolon],
+                    "test2"),
+                expected: format!("Unexpected token while parsing {} - got: {}, expected: {}",
+                    "test2",
+                    ParsingError::display_option_token(&Some(Token::Identifier("barbaz".to_string()))),
+                    ParsingError::display_vec_token(&vec![Token::Comma, Token::Semicolon])),
+            },
+            Test {
+                input: ParsingError::new_unexpected(
+                    Some(&Token::Bang),
+                    vec![],
+                    "test3"),
+                expected: format!("Unexpected token while parsing {} - got: {}, expected: {}",
+                    "test3",
+                    ParsingError::display_option_token(&Some(Token::Bang)),
+                    ParsingError::display_vec_token(&vec![])),
+            },
+            
+            //multiple errors
+            Test {
+                input: ParsingError::new_multiple(vec![]),
+                expected: format!("Multiple errors - len={}", 0),
+            },
+            Test {
+                input: ParsingError::new_multiple(vec![
+                    ParsingError::new_unexpected(Some(&Token::Bang), vec![Token::Equals, Token::NotEquals], "test5"),
+                ]),
+                expected: format!("Multiple errors - len={}", 1),
+            },
+            Test {
+                input: ParsingError::new_multiple(vec![
+                    ParsingError::new_other("some other error in test6a"),
+                    ParsingError::new_unexpected(Some(&Token::LeftCurly), vec![Token::RightCurly], "test6b"),
+                ]),
+                expected: format!("Multiple errors - len={}", 2),
+            },
+
+            //other
+            Test {
+                input: ParsingError::new_other(""),
+                expected: "Other error".to_string(),
+            },
+            Test {
+                input: ParsingError::new_other("some other error in test8"),
+                expected: "Other error: some other error in test8".to_string(),
+            }
+        ];
+
+        for Test{input, expected} in tests {
+            assert_eq!(input.to_string(), expected);
+        }
+    }
+}
 /*
  --------------------- expected something specific ---------------
 --
