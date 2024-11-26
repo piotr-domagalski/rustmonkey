3dd1c4bb8f8c4b007869574eda6e290e81b23825
Piotr Domagalski <pdomagalski02@gmail.com>
2024-11-26 15:40:38 +0100

    rppl

--- src/repl.rs
+++ src/repl.rs
@@ -1,10 +1,19 @@
 use text_io::read;
 use crate::lexer::Lexer;
+use crate::ast::Statement;
 
+#[allow(unused_must_use)]
 pub fn start() {
     loop {
         print!(">>> ");
         let input: String = read!("{}\n");
         dbg!(Lexer::new(&input).collect::<Vec<_>>());
+        let lexer =  Lexer::new(&input);
+        let result = Statement::parse(&mut lexer.peekable());
+        match result {
+            Ok(Statement::Expression { expression }) => { println!("{};", expression); },
+            Ok(statement) => { dbg!(statement); },
+            Err(err) => { println!("{}", err); },
+        }
     }
 } 
